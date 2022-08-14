6519c7e9695316c87ad32e212626065dc80de243
GirtsRimis <girts60@gmail.com>
2022-08-14 14:30:05 +0300

    add custom atoi to main

--- main.cpp
+++ main.cpp
@@ -32,9 +32,28 @@ bool is_big_endian(void)
     return bint.c[0] == 1;
 }
 
+int myAtoi(char* str)
+{
+    // Initialize result
+    int res = 0;
+ 
+    // Iterate through all characters
+    // of input string and update result
+    // take ASCII character of corresponding digit and
+    // subtract the code from '0' to get numerical
+    // value and multiply res by 10 to shuffle
+    // digits left to update running total
+    for (int i = 0; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+ 
+    // return result.
+    return res;
+}
+
 int main()
 {
     std::cout << "Is big endian: " << is_big_endian() << " - 0 (false), 1 (true)\n";
+    std::cout << "100 to int: " << myAtoi("100") << "\n";
 
     return 0;
 }
